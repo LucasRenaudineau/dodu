59415caf94301d21b462536caf831058b08402c2
Gwendal Grison <gwendal.grison@laposte.net>
2025-11-23 00:32:08 +0100

    added dash

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -2,14 +2,22 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
 	public float force_amount = 10;
-	private Rigidbody2D rb; // removed "public" - can't be both private and public
-	private float keys_pressed = 0;
+	private Rigidbody2D rb;
 	private int vertic = 0;
 	private int horiz = 0;
 	public float k=0.3f; // dry friction coefficient
+	public float dashIntensity=10.0f;
+	public float deltaDash=5.0f; // time between dashes
+	private float timeSinceLastDash=0f;
+	private bool dashAvailable=true;
+	private bool justDashed=false;
+	public float dashDuration=0.5f;
+	private Vector2 dir = Vector2.zero;
+	private Vector2 prevDir = Vector2.zero;
 	// Start is called once before the first execution of Update after the MonoBehaviour is created
 	void Start() {
 		rb = GetComponent<Rigidbody2D>();
+		timeSinceLastDash=deltaDash;
        	}
 	// Update is called once per frame
 	void Update() {
@@ -24,7 +32,31 @@ public class PlayerMovement : MonoBehaviour
 		} if (Input.GetKey(KeyCode.RightArrow)) {
 			horiz +=1;
 		}
-		rb.AddForce((new Vector2(horiz,vertic)).normalized*force_amount, ForceMode2D.Impulse);
-		rb.AddForce(-k*rb.linearVelocity);
+		dir=(new Vector2(horiz,vertic)).normalized;
+		if (dir != Vector2.zero && !dashAvailable) {
+			timeSinceLastDash+=Time.deltaTime;
+			if (timeSinceLastDash>deltaDash) {
+				dashAvailable=true;
+			}
+		}
+		if (Input.GetKeyDown(KeyCode.F) && dashAvailable) {
+			dashAvailable=false;
+			timeSinceLastDash=0f;
+			prevDir=dir;
+		}
+	}
+	void FixedUpdate() {
+		if (timeSinceLastDash<=dashDuration) {
+			rb.AddForce(prevDir * dashIntensity, ForceMode2D.Impulse);
+			justDashed=true;
+		}
+		else if (justDashed) {
+			rb.linearVelocity=dir*force_amount;
+			justDashed=false;
+		}
+		else {
+			rb.AddForce(dir*force_amount);
+			rb.AddForce(-k*rb.linearVelocity);
+		}
 	}
 }
