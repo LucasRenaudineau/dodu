19f0d44473e9ebcbd8885fd092f5b3b657c187ba
Gwendal Grison <gwendal.grison@laposte.net>
2025-11-22 22:18:03 +0100

    simplified the code

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -1,51 +1,28 @@
 using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
-		public float force_amount = 10;
-		private Rigidbody2D rb; // removed "public" - can't be both private and public
-		private float keys_pressed = 0;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-			rb = GetComponent<Rigidbody2D>();
-			keys_pressed = 0;
-        
-    }
-    // Update is called once per frame
-    void Update()
-    {
-			keys_pressed = 0;
-			if (Input.GetKey(KeyCode.UpArrow)) {
-					keys_pressed +=1;
+	public float force_amount = 10;
+	private Rigidbody2D rb; // removed "public" - can't be both private and public
+	private float keys_pressed = 0;
+	private int vertic = 0;
+	private int horiz = 0;
+	// Start is called once before the first execution of Update after the MonoBehaviour is created
+	void Start() {
+		rb = GetComponent<Rigidbody2D>();
+       	}
+	// Update is called once per frame
+	void Update() {
+		vertic = 0;
+		horiz = 0;
+		if (Input.GetKey(KeyCode.UpArrow)) {
+			vertic +=1;
 		} if (Input.GetKey(KeyCode.DownArrow)) {
-					keys_pressed +=1;
+			vertic -=1;
 		} if (Input.GetKey(KeyCode.LeftArrow)) {
-					keys_pressed +=1;
+			horiz -=1;
 		} if (Input.GetKey(KeyCode.RightArrow)) {
-					keys_pressed +=1;
+			horiz +=1;
 		}
-			if (keys_pressed==1) {
-					if (Input.GetKey(KeyCode.UpArrow)) {
-							rb.AddForce(new Vector2(0, force_amount), ForceMode2D.Impulse);
-					} else if (Input.GetKey(KeyCode.DownArrow)) {
-							rb.AddForce(new Vector2(0, -force_amount), ForceMode2D.Impulse);
-					} else if (Input.GetKey(KeyCode.LeftArrow)) {
-							rb.AddForce(new Vector2(-force_amount, 0), ForceMode2D.Impulse);
-					} else if (Input.GetKey(KeyCode.RightArrow)) {
-							rb.AddForce(new Vector2(force_amount, 0), ForceMode2D.Impulse);
-					}
-		}
-		else if (keys_pressed>1) {
-				if (Input.GetKey(KeyCode.UpArrow)) {
-								rb.AddForce(new Vector2(0, force_amount/Mathf.Sqrt(2)), ForceMode2D.Impulse);
-				} if (Input.GetKey(KeyCode.DownArrow)) { // Added closing parenthesis
-								rb.AddForce(new Vector2(0, -force_amount/Mathf.Sqrt(2)), ForceMode2D.Impulse);
-				} if (Input.GetKey(KeyCode.LeftArrow)) { // Added closing parenthesis
-								rb.AddForce(new Vector2(-force_amount/Mathf.Sqrt(2), 0), ForceMode2D.Impulse);
-				} if (Input.GetKey(KeyCode.RightArrow)) { // Added closing parenthesis
-								rb.AddForce(new Vector2(force_amount/Mathf.Sqrt(2), 0), ForceMode2D.Impulse);
-				}
-		}
-        
-    }
+		rb.AddForce((new Vector2(horiz,vertic)).normalized*force_amount, ForceMode2D.Impulse);
+	}
 }
