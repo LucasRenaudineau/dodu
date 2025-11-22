7577598190a742c153c57b2884353de6c2e4768d
Lucas Renaudineau <lucas.renaudineau@telecom-paris.fr>
2025-11-22 23:40:38 +0100

    added big obstacle

---
+++ Assets/BigObstacleSpawner.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BigObstacleSpawner : MonoBehaviour {
+    [SerializeField] private GameObject projectilePrefab;
+    [SerializeField] private float projectileSpeed = 10000;
+    [SerializeField] private float projectileLifetime = 50;
+    [SerializeField] private float spawnInterval = 4;
+	[SerializeField] private float initialDistance = 100;
+    private float timer = 0f;
+
+    void Start() {
+    }
+
+    void SpawnProjectile() {
+        float random_orientation = Random.Range(0, 360);
+        Quaternion rotation = Quaternion.Euler(0, 0, random_orientation);
+        
+        float x = - Mathf.Cos(random_orientation * Mathf.Deg2Rad) * initialDistance;
+        float y = - Mathf.Sin(random_orientation * Mathf.Deg2Rad) * initialDistance;
+        Vector3 spawnPosition = new Vector3(x, y, 0) + transform.position;
+        
+        GameObject projectile = Instantiate(projectilePrefab, spawnPosition, rotation);
+        
+        Vector3 direction = (transform.position - spawnPosition).normalized;
+        
+        BigObstacleLife obstacleScript = projectile.GetComponent<BigObstacleLife>();
+        if (obstacleScript != null) {
+            obstacleScript.Initialize(direction, projectileSpeed, projectileLifetime);
+        }
+    }
+
+    void Update() {
+        timer += Time.deltaTime;
+        if (timer >= spawnInterval) {
+            SpawnProjectile();
+            timer = 0;
+        }
+    }
+}
