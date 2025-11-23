c3331f32bd10c8704345a02ca77af611da134ba1
Lucas Renaudineau <lucas.renaudineau@telecom-paris.fr>
2025-11-23 12:24:42 +0100

    small Obstacles and small Obstacles spawner

--- Assets/SmallObstacleSpawner.cs
+++ Assets/SmallObstacleSpawner.cs
@@ -2,11 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BigObstacleSpawner : MonoBehaviour {
+public class SmallObstacleSpawner : MonoBehaviour {
 		[SerializeField] private GameObject projectilePrefab;
-		[SerializeField] private float projectileSpeed = 1000;
-		[SerializeField] private float projectileLifetime = 30;
-		[SerializeField] private float spawnInterval = 4;
+		[SerializeField] private float projectileSpeed = 2000;
+		[SerializeField] private float projectileLifetime = 10;
+		[SerializeField] private float spawnInterval = 2;
 		[SerializeField] private float initialDistance = 3000;
 		private float timer = 0f;
 		void Start()
@@ -19,15 +19,15 @@ public class BigObstacleSpawner : MonoBehaviour {
         float random_orientation = Random.Range(0, 360);
         Quaternion rotation = Quaternion.Euler(0, 0, random_orientation);
         
-        float x = - Mathf.Cos(random_orientation * Mathf.Deg2Rad) * initialDistance + Random.Range(-1200,1200);
-        float y = - Mathf.Sin(random_orientation * Mathf.Deg2Rad) * initialDistance + Random.Range(-1200,1200);
+        float x = - Mathf.Cos(random_orientation * Mathf.Deg2Rad) * initialDistance + Random.Range(-800,800);
+        float y = - Mathf.Sin(random_orientation * Mathf.Deg2Rad) * initialDistance + Random.Range(-800,800);
         Vector3 spawnPosition = new Vector3(x, y, 0) + transform.position;
         
         GameObject projectile = Instantiate(projectilePrefab, spawnPosition, rotation);
         
         Vector2 direction = (transform.position - spawnPosition).normalized;
         
-        BigObstacle obstacleScript = projectile.GetComponent<BigObstacle>();
+        SmallObstacle obstacleScript = projectile.GetComponent<SmallObstacle>();
 		if (obstacleScript != null) {
 				obstacleScript.Initialize(direction, projectileSpeed, projectileLifetime);
 		}
