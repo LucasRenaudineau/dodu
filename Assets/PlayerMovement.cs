7a3ecbb5a226535d4301233a2c1da2338fce6af5
Pierre Neillo <pierre.neillo@free.fr>
2025-11-23 02:28:57 +0100

    Comments and collision reparation

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -1,62 +1,57 @@
 using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
-	public float force_amount = 10;
+	public float force_amount = 8;
 	private Rigidbody2D rb;
-	private int vertic = 0;
-	private int horiz = 0;
 	public float k=0.3f; // dry friction coefficient
 	public float dashIntensity=10.0f;
 	public float deltaDash=5.0f; // time between dashes
 	private float timeSinceLastDash=0f;
 	private bool dashAvailable=true;
-	private bool justDashed=false;
 	public float dashDuration=0.5f;
-	private Vector2 dir = Vector2.zero;
-	private Vector2 prevDir = Vector2.zero;
+	private Vector2 direction = Vector2.zero;
+	private Vector2 dashDirection = Vector2.zero;
 	// Start is called once before the first execution of Update after the MonoBehaviour is created
 	void Start() {
 		rb = GetComponent<Rigidbody2D>();
-		timeSinceLastDash=deltaDash;
        	}
 	// Update is called once per frame
 	void Update() {
-		vertic = 0;
-		horiz = 0;
-		if (Input.GetKey(KeyCode.UpArrow)) {
-			vertic +=1;
-		} if (Input.GetKey(KeyCode.DownArrow)) {
-			vertic -=1;
+		direction = new Vector2(0,0);
+		// We set current direction vector according to user's inputs
+		if (Input.GetKey(KeyCode.UpArrow))
+        {
+            direction.y = 1;
+        } if (Input.GetKey(KeyCode.DownArrow)) {
+			direction.y = -1;
 		} if (Input.GetKey(KeyCode.LeftArrow)) {
-			horiz -=1;
+			direction.x = -1;
 		} if (Input.GetKey(KeyCode.RightArrow)) {
-			horiz +=1;
+			direction.x = 1;
 		}
-		dir=(new Vector2(horiz,vertic)).normalized;
-		if (dir != Vector2.zero && !dashAvailable) {
+		// Normalising direction
+		direction = direction.normalized;
+		Debug.Log(direction);
+		if (!dashAvailable) {
+			// If the dash is not available, we count time until it will be
 			timeSinceLastDash+=Time.deltaTime;
 			if (timeSinceLastDash>deltaDash) {
+				// If it has been enough time, dash is available 
 				dashAvailable=true;
 			}
 		}
 		if (Input.GetKeyDown(KeyCode.F) && dashAvailable) {
+			// If users asks for a dash and it is available, we dash
 			dashAvailable=false;
 			timeSinceLastDash=0f;
-			prevDir=dir;
+			dashDirection=direction;
 		}
 	}
 	void FixedUpdate() {
 		if (timeSinceLastDash<=dashDuration) {
-			rb.AddForce(prevDir * dashIntensity, ForceMode2D.Impulse);
-			justDashed=true;
+			rb.AddForce(dashDirection * dashIntensity, ForceMode2D.Impulse);
 		}
-		else if (justDashed) {
-			rb.linearVelocity=dir*force_amount;
-			justDashed=false;
-		}
-		else {
-			rb.AddForce(dir*force_amount);
-			rb.AddForce(-k*rb.linearVelocity);
-		}
+		rb.AddForce(direction*force_amount);
+		rb.AddForce(-k*rb.linearVelocity);
 	}
 }
