0d4e917e0f0c3987080790d2f0ddbf0c798e604b
Gwendal Grison <gwendal.grison@laposte.net>
2025-11-23 16:34:09 +0100

    fixed player 2 movements

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -45,9 +45,9 @@ public class PlayerMovement : MonoBehaviour
 		else {
 			if (Input.GetKey(KeyCode.W)) {
                                 direction.y = 1;
-                        } if (Input.GetKey(KeyCode.A)) {
-                                direction.y = -1;
                         } if (Input.GetKey(KeyCode.S)) {
+                                direction.y = -1;
+                        } if (Input.GetKey(KeyCode.A)) {
                                 direction.x = -1;
                         } if (Input.GetKey(KeyCode.D)) {
                                 direction.x = 1;
