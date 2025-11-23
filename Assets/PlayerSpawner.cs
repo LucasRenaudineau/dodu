37db721f794cbbe93fb9a46ccec33e64b0a7ba5a
Gwendal Grison <gwendal.grison@laposte.net>
2025-11-23 13:09:01 +0100

    added a second player, need to fix its movements

---
+++ Assets/PlayerSpawner.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class PlayerSpawner : MonoBehaviour
+{
+	public GameObject playerPrefab;
+	public Vector2 startPosP1 = new Vector2(-25, 0);
+	public Vector2 startPosP2 = new Vector2(25, 0);
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+	 void Start()
+   	 {
+		SpawnPlayers();
+	 }
+
+   	 // Update is called once per frame
+   	 void SpawnPlayers() {
+	 	GameObject p1 = Instantiate(playerPrefab, startPosP1, Quaternion.identity);
+		PlayerMovement scriptP1 = p1.GetComponent<PlayerMovement>();
+		scriptP1.Initialize(1);
+		GameObject p2 = Instantiate(playerPrefab, startPosP2, Quaternion.identity);
+                PlayerMovement scriptP2 = p2.GetComponent<PlayerMovement>();
+                scriptP2.Initialize(2);
+	 }
+}
